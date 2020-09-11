c56e0a297faafa252e0c0fdd57aa50fd093306bc
Gh0stM0de <=jdotcole233@gmail.com>
2020-09-11 10:39:56 +0000

    config data

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -3,10 +3,10 @@ export const PROTOCOL =
   process.env.NODE_ENV === "development" ? "http://" : "https://";
 export const DOMAIN =
   process.env.NODE_ENV === "development"
-    ? "192.168.100.6:8000"
-    : "manage.visalreinsurance.com";
+    ? "192.168.100.7:8000"
+    : "manage.comfybroker.com";
 export const COOKIE_NAME_AUTH_TOKEN = "visal_re_auth_token";
-export const PUSHER_KEY = "6732a1bd9862dfe49492";
+export const PUSHER_KEY = "8fdc449c93173e31d8a2";
 export const PUSHER_CLUSTER = "eu";
-export const CHANNEL = "LgzULSz64aed7WJ7YwDq3qmHAeX786dZNZ3Ko4Bisf0";
-export const EVENT = "LgzULSz64aed7WJ7YwDq3qmHAeX786dZNZ3Ko4Bisf0-event";
+export const CHANNEL = "a2VrcmUtcHJpdmF0ZS1jaGFubmVs";
+export const EVENT = "a2VrcmUtcHJpdmF0ZS1jaGFubmVs-event";
