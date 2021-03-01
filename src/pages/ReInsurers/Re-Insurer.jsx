503eef83c7ebfd68ca959e7dd122a0676e62098d
Khoby-790 <ebaidoo79@gmail.com>
2021-03-01 10:59:02 +0000

    --- Auto-Git Commit ---

--- src/pages/ReInsurers/Re-Insurer.jsx
+++ src/pages/ReInsurers/Re-Insurer.jsx
@@ -69,11 +69,13 @@ function ReInsurer({ data, openAssociateModal }) {
                     <h5 className="font-size-15"><a href="#" className="text-dark">{data.re_company_name}</a></h5>
                 </div>
                 <div className="card-footer bg-transparent border-top">
-                    <div onClick={() => onSelectReinsurer(data)} className="contact-links d-flex font-size-20">
-                        <a className="flex-fill link-hover" data-toggle="tooltip" data-placement="top" title="View"><i className="bx bx-pie-chart-alt"></i></a>
-                        {create_reinsurer_access.includes(user?.position) && <div className="flex-fill link-hover" onClick={() => openAssociateModal(data, !0)}>
-                            <a data-toggle="tooltip" data-placement="top" title="Add Associate"><i className="bx bx-user-circle"></i></a>
-                        </div>}
+                    <div  className="contact-links d-flex font-size-20">
+                        <a onClick={() => onSelectReinsurer(data)} className="flex-fill link-hover" data-toggle="tooltip" data-placement="top" title="View"><i className="bx bx-pie-chart-alt"></i></a>
+                        {create_reinsurer_access.includes(user?.position) &&
+                            <div className="flex-fill link-hover" onClick={() => openAssociateModal(data, !0)}>
+                                <a data-toggle="tooltip" data-placement="top" title="Add Associate"><i className="bx bx-user-circle"></i></a>
+                            </div>
+                        }
                         {delete_reinsurer_access.includes(user?.position) && <div onClick={() => handleDeleteReinsurer(data)} className="flex-fill link-hover">
                             <i className="bx bx-trash-alt"></i>
                         </div>}
