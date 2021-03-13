3f4484f4f980d32ac103960534245fede42c3a4f
Khoby-790 <ebaidoo79@gmail.com>
2021-03-13 11:17:52 +0000

    --- Auto-Git Commit ---

--- src/pages/Insurers/DistributePayment.jsx
+++ src/pages/Insurers/DistributePayment.jsx
@@ -7,6 +7,7 @@ import { useMutation, useQuery } from "react-apollo";
 import { DISTRIBUTE_PAYMENT } from "../../graphql/mutattions";
 import { INSURER, GET_ISNURER_DEDUCTIONS } from "../../graphql/queries";
 import { Alert } from "react-bootstrap";
+import DistributePaymentForm from "./components/DistributePaymentForm";
 
 export default function DistributePayment({
   data,
@@ -144,188 +145,15 @@ export default function DistributePayment({
               ? forms.map((participant, key) => {
                   return !data?.offer_participant[key]
                     .offer_participant_percentage ? null : (
-                    <fieldset className="border-form p-2 mb-2" key={key}>
-                      <legend className={styles.details_title}>
-                        {
-                          data?.offer_participant[key]?.reinsurer
-                            ?.re_company_name
-                        }
-                      </legend>
-                      <div className="row">
-                        <div className="col-md-12">
-                          {data?.offer_participant[key]
-                            ?.offer_participant_payment?.length ? (
-                            <table className="table border border-dark table-bordered">
-                              <tbody>
-                                <tr>
-                                  <th>Facultative Premuim</th>
-                                  <td>
-                                    {data?.offer_participant[
-                                      key
-                                    ].participant_fac_premium?.toLocaleString(
-                                      undefined,
-                                      { maximumFractionDigits: 2 }
-                                    )}
-                                  </td>
-                                  <th>Withholding Tax</th>
-                                  <td>
-                                    {reinsurers[key]?.offer_deductions[
-                                      reinsurers[key]?.offer_deductions.length -
-                                        1
-                                    ]?.withholding_tax_paid?.toLocaleString(
-                                      undefined,
-                                      { maximumFractionDigits: 2 }
-                                    )}
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th>Participating %</th>
-                                  <td>
-                                    {
-                                      data?.offer_participant[key]
-                                        .offer_participant_percentage
-                                    }
-                                  </td>
-                                  <th>Brokerage</th>
-                                  <td>
-                                    {reinsurers[key]?.offer_deductions[
-                                      reinsurers[key]?.offer_deductions.length -
-                                        1
-                                    ]?.brokerage_amount_paid?.toLocaleString(
-                                      undefined,
-                                      { maximumFractionDigits: 2 }
-                                    )}
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th>NIC levy</th>
-                                  <td>
-                                    {reinsurers[key]?.offer_deductions[
-                                      reinsurers[key]?.offer_deductions.length -
-                                        1
-                                    ]?.nic_levy_paid?.toLocaleString(
-                                      undefined,
-                                      { maximumFractionDigits: 2 }
-                                    )}
-                                  </td>
-                                  <th>Commission taken</th>
-                                  <td>
-                                    {reinsurers[key]?.offer_deductions[
-                                      reinsurers[key]?.offer_deductions.length -
-                                        1
-                                    ]?.commission_taken?.toLocaleString(
-                                      undefined,
-                                      { maximumFractionDigits: 2 }
-                                    )}
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <th></th>
-                                  <td></td>
-                                  <th>Amount</th>
-                                  <td>
-                                    {data?.offer_participant[
-                                      key
-                                    ].offer_participant_payment[
-                                      data?.offer_participant[key]
-                                        .offer_participant_payment.length - 1
-                                    ].offer_payment_amount?.toLocaleString(
-                                      undefined,
-                                      { maximumFractionDigits: 2 }
-                                    )}
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                          ) : null}
-                        </div>
-                      </div>
-                      <div className="form-group">
-                        <label htmlFor="payment_type">Payment Type</label>
-                        <select
-                          name="payment_type"
-                          id=""
-                          value={participant.payment_type}
-                          onChange={(e) => handleChange(key, e)}
-                          className="form-control"
-                        >
-                          <option value="">Select payment type</option>
-                          <option value="Bank Transfer">Bank Transfer</option>
-                          <option value="Cheque">Cheque</option>
-                        </select>
-                        {errors[key].payment_type.type && (
-                          <p>{errors[key].payment_type.message}</p>
-                        )}
-                      </div>
-
-                      {/* Banks section */}
-                      <div className="row">
-                        {participant.payment_type && (
-                          <div className="col-md-6">
-                            <div className="form-group">
-                              <label htmlFor="bank name">Bank Name</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                value={participant.bank_name}
-                                onChange={(e) => handleChange(key, e)}
-                                name="bank_name"
-                              />
-                            </div>
-                          </div>
-                        )}
-                        {participant.payment_type === "Bank Transfer" && (
-                          <div className="col-md-6">
-                            <div className="form-group">
-                              <label htmlFor="bank name">
-                                Beneficiary Bank Name
-                              </label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                value={participant.b_bank_name}
-                                name="b_bank_name"
-                                onChange={(e) => handleChange(key, e)}
-                              />
-                            </div>
-                          </div>
-                        )}
-                        {participant.payment_type === "Cheque" && (
-                          <div className="col-md-6">
-                            <div className="form-group">
-                              <label htmlFor="bank name">Cheque Number</label>
-                              <input
-                                type="text"
-                                className="form-control"
-                                value={participant.cheque_number}
-                                name="cheque_number"
-                                onChange={(e) => handleChange(key, e)}
-                              />
-                            </div>
-                          </div>
-                        )}
-                      </div>
-                      {/* End of Bank section */}
-
-                      {/* Cheque section */}
-                      <div className="row"></div>
-                      {/* End of cheque section */}
-
-                      {/* Comment section */}
-                      <div className="form-group">
-                        <label htmlFor="comment">Comment</label>
-                        <textarea
-                          className="form-control"
-                          value={participant.comment}
-                          onChange={(e) => handleChange(key, e)}
-                          name="comment"
-                          id=""
-                          cols="30"
-                          rows="3"
-                        ></textarea>
-                      </div>
-                      {/* End of comment section */}
-                    </fieldset>
+                    <DistributePaymentForm
+                      key={key}
+                      index={key}
+                      data={data}
+                      reinsurers={reinsurers}
+                      participant={participant}
+                      errors={errors}
+                      handleChange={handleChange}
+                    />
                   );
                 })
               : null}
