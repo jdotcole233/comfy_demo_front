927c5d6b23e76babd38fa0f37ed8f49cd24585de
Khoby-790 <ebaidoo79@gmail.com>
2021-03-13 11:28:48 +0000

    --- Auto-Git Commit ---

--- src/pages/Insurers/DistributePayment.jsx
+++ src/pages/Insurers/DistributePayment.jsx
@@ -118,7 +118,7 @@ export default function DistributePayment({
 
   if (showFlag) {
     return (
-      <div className="h-full bg-primary flex-1">
+      <div className="mh-100 h-full bg-primary flex-1">
         <Alert variant="danger">
           <strong>
             No current payment identified on offer with policy number:{" "}
