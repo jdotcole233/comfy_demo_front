f74b016537489e680ec0de4e2c37cb3bbad43b82
Khoby-790 <ebaidoo79@gmail.com>
2021-04-24 12:55:53 +0000

    --- Auto-Git Commit ---

--- src/pages/Claims/actions.js
+++ src/pages/Claims/actions.js
@@ -13,25 +13,59 @@ export const generateClaimsTable = ({
     sum_insured:
       offer.offer_detail?.currency +
       ' ' +
-      offer.sum_insured.toLocaleString(undefined, {maximumFractionDigits: 2}),
+      offer.sum_insured.toLocaleString(undefined, { maximumFractionDigits: 2 }),
     insurance_company: offer.insurer.insurer_company_name,
     premium:
       offer.offer_detail?.currency +
       ' ' +
-      offer.premium.toLocaleString(undefined, {maximumFractionDigits: 2}),
+      offer.premium.toLocaleString(undefined, { maximumFractionDigits: 2 }),
     participants: offer.offer_participant.length,
     claim_status: (
       <span
-        style={{letterSpacing: 3}}
-        className={`badge badge-${
-          offer.claim_status === 'CLAIMED' ? 'danger' : 'primary'
-        }`}
+        style={{ letterSpacing: 3 }}
+        className={`badge badge-${offer.claim_status === 'CLAIMED' ? 'danger' : 'primary'
+          }`}
       >
         {offer.claim_status}
       </span>
     ),
     cob: offer.classofbusiness.business_name,
     offer_date: new Date(offer.created_at).toDateString(),
+    endorsements: offer?.offer_endorsements?.length ? (
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="font-size-16 text-success"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M5 13l4 4L19 7"
+          />
+        </svg>
+      </span>
+    ) : (
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="font-size-16 text-danger"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </span>
+    ),
     actions: (
       <>
         {offer?.offer_claims?.length ? (
