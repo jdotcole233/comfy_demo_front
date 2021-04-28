9718337c8f93ce3ba284c6552b2c108de84dbe13
Khoby-790 <ebaidoo79@gmail.com>
2021-04-28 21:35:49 +0000

    --- Auto-Git Commit ---

--- src/pages/CreateClosing/EndorsementPreviews/EndorsementDebitNote.jsx
+++ src/pages/CreateClosing/EndorsementPreviews/EndorsementDebitNote.jsx
@@ -257,7 +257,11 @@ function EndorsementDebitNote({ offer, endorsement, doc_number }) {
               <div className="col-md-6 col-8 col-sm-8 col-xs-8">
                 <h3 className="dark-text-value">
                   {offer?.exchange_rate?.ex_currency}{" "}
-                  {endorsement?.commission_amount.toLocaleString(undefined, {
+                  {getPremium(
+                    offer,
+                    doc_number,
+                    "commission_amount"
+                  ).toLocaleString(undefined, {
                     maximumFractionDigits: 2,
                   })}
                 </h3>
