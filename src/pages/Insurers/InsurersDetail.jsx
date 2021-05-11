c73f39f5f74ee49d5712e66c97b98978c5d04010
Khoby-790 <ebaidoo79@gmail.com>
2021-05-11 16:41:40 +0000

    --- Auto-Git Commit ---

--- src/pages/Insurers/InsurersDetail.jsx
+++ src/pages/Insurers/InsurersDetail.jsx
@@ -166,17 +166,55 @@ function InsurerDetail() {
     () =>
       insurer_offers?.insurer_all_offers?.offers.map((offer, i) => ({
         name: offer.offer_detail?.policy_number,
+        currency: offer?.offer_detail?.currency,
+        outstanding: (expected - payments_made).toLocaleString(undefined, {
+          maximumFractionDigits: 2,
+        }),
+        expected_premium: expected.toLocaleString(undefined, {
+          maximumFractionDigits: 2,
+        }),
         insured: offer.offer_detail?.insured_by,
-        sum_insured: `${
-          offer?.offer_detail?.currency
-        } ${offer.sum_insured.toLocaleString(undefined, {
+        sum_insured: ` ${offer.sum_insured.toLocaleString(undefined, {
           maximumFractionDigits: 2,
         })}`,
-        f_sum_insured: `${
-          offer?.offer_detail?.currency
-        } ${offer.fac_sum_insured.toLocaleString(undefined, {
+        f_sum_insured: ` ${offer.fac_sum_insured.toLocaleString(undefined, {
           maximumFractionDigits: 2,
         })}`,
+        endorsements: offer?.offer_endorsements?.length ? (
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="font-size-16 text-success"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+          </span>
+        ) : (
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="font-size-16 text-danger"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </span>
+        ),
         comission: offer.commission,
         cob: offer.classofbusiness.business_name,
         offer_date: offer.created_at,
