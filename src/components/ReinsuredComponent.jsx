e87fda9c08b8c7019e67e83cc28773c86e23b565
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 14:23:33 +0000

    --- Auto-Git Commit ---

--- src/components/ReinsuredComponent.jsx
+++ src/components/ReinsuredComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ReinsuredComponent = ({ name }) => (
   <span>
     {name}
-    <sup className="text-danger  font-weight-bold">Retro</sup>
+    <sup className="text-danger ml-2 font-weight-bold">RETRO</sup>
   </span>
 );
 
