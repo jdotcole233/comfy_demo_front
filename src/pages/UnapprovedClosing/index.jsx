f03a762eb4a2f7587db1f85e0550fa48813f08a4
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 14:29:01 +0000

    --- Auto-Git Commit ---

--- src/pages/UnapprovedClosing/index.jsx
+++ src/pages/UnapprovedClosing/index.jsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import { columns, _columns } from "./columns";
 import { useQuery } from "react-apollo";
 import { OFFERS } from "../../graphql/queries";
-import { Datatable, isWithinAWeek, Loader } from "../../components";
+import { Datatable, isWithinAWeek, Loader, ReinsuredComponent } from "../../components";
 import OfferButtons from "./components/OfferButtons";
 import { ENDORSEMENTS } from "../../graphql/queries/endorsements";
 import EndorsementButtons from "./components/EndorsementButtons";
@@ -37,7 +37,13 @@ const UnApproved = () => {
           offer.sum_insured.toLocaleString(undefined, {
             maximumFractionDigits: 2,
           }),
-        insurance_company: offer.insurer.insurer_company_name,
+          insurance_company: offer?.offer_retrocedent ? (
+            <ReinsuredComponent
+              name={offer?.offer_retrocedent?.reinsurer?.re_company_name}
+            />
+          ) : (
+            offer.insurer.insurer_company_name
+          ),
         premium:
           offer.offer_endorsement_detail?.currency +
           " " +
