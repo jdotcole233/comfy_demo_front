e2804dce97a16d3bb08d1e132e428311816083b3
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 14:46:29 +0000

    --- Auto-Git Commit ---

--- src/pages/ReInsurers/ReinsurerDetail.jsx
+++ src/pages/ReInsurers/ReinsurerDetail.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-throw-literal */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { Link, useHistory } from "react-router-dom";
 import {
   Drawer,
@@ -197,6 +197,63 @@ function ReinsurerDetail() {
       });
       setOfferListing(offers);
     }
+  }, [data]);
+
+  const broughtInOffers = useMemo(() => {
+    if (data && data.reinsurer && data.reinsurer.offer_retrocedents) {
+      return data.reinsurer.offer_retrocedents.map((offer) => {
+        const row = {
+          policy_no: offer.reinsurer_offers_only.offer_detail.policy_number,
+          company: offer.reinsurer_offers_only.insurer.insurer_company_name,
+          cob: offer.reinsurer_offers_only.classofbusiness.business_name,
+          participating_percentage: offer.offer_participant_percentage,
+          fac_sum_insured:
+            offer.reinsurer_offers_only.fac_sum_insured.toLocaleString(
+              undefined,
+              { maximumFractionDigits: 2 }
+            ),
+          fac_premium: offer.reinsurer_offers_only.fac_premium.toLocaleString(
+            undefined,
+            { maximumFractionDigits: 2 }
+          ),
+          offer_status: (
+            <span
+              style={{ letterSpacing: 3 }}
+              className={`badge badge-${
+                offer.reinsurer_offers_only.offer_status === "OPEN"
+                  ? "primary"
+                  : offer.reinsurer_offers_only.offer_status === "PENDING"
+                  ? "danger"
+                  : "success"
+              } font-size-11`}
+            >
+              {offer.reinsurer_offers_only.offer_status}
+            </span>
+          ),
+          brokerage: offer.reinsurer_offers_only.brokerage,
+          payment_status: (
+            <span
+              style={{ letterSpacing: 3 }}
+              className={`badge badge-${
+                offer.reinsurer_offers_only.payment_status === "PART PAYMENT"
+                  ? "primary"
+                  : offer.reinsurer_offers_only.payment_status === "UNPAID"
+                  ? "danger"
+                  : "success"
+              } font-size-11`}
+            >
+              {offer.reinsurer_offers_only.payment_status}
+            </span>
+          ),
+          offer_date: offer.reinsurer_offers_only.created_at,
+          insured: offer.reinsurer_offers_only.offer_detail.insured_by,
+          actions: <OfferButtons data={data} offer={offer} />,
+        };
+        return row;
+      });
+    }
+
+    return [];
   }, [data]);
 
   const handleLoadMore = (skip) => {
@@ -409,7 +466,6 @@ function ReinsurerDetail() {
         />
 
         {/* Sections for Offers brought in by Reinsurers company */}
-        
 
         <div className="">
           <div className="col-md-12">
