f4771d0fa64e06621e421d036d2863da08df7224
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 14:19:10 +0000

    --- Auto-Git Commit ---

--- src/pages/CreateSlip/index.jsx
+++ src/pages/CreateSlip/index.jsx
@@ -12,7 +12,7 @@ import { columns } from "./columns";
 const ReinsuredComponent = ({ name }) => (
   <>
     {name}
-    <span className="text-danger font-size-16">*</span>
+    <span className="text-danger font-size-16 font-weight-bold">*</span>
   </>
 );
 
