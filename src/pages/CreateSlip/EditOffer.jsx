d852e5d0a39151b4378eccd33548b7bc07da5448
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 13:20:12 +0000

    --- Auto-Git Commit ---

--- src/pages/CreateSlip/EditOffer.jsx
+++ src/pages/CreateSlip/EditOffer.jsx
@@ -67,9 +67,9 @@ export default function InputOffer({ offer_id, toggle }) {
 
   useEffect(() => {
     if (offer) {
-      const selectedPayment_type = Object.keys(
+      const selectedPayment_type = offer?.offer_detail?.payment_type ? Object.keys(
         JSON.parse(offer?.offer_detail?.payment_type)
-      )?.[0];
+      )[0]: "payable_in_full";
       setAddExchangeRate(offer.exchange_rate ? true : false);
       setValue("policy_number", offer.offer_detail?.policy_number); //insurance_company
       setValue("insurer_id", offer.insurer?.insurer_id); //insurance_company
