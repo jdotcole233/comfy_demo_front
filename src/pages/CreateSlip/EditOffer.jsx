c4bef3d5eb1299e7b61ba10015d46c934e51426d
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 13:02:43 +0000

    --- Auto-Git Commit ---

--- src/pages/CreateSlip/EditOffer.jsx
+++ src/pages/CreateSlip/EditOffer.jsx
@@ -114,6 +114,12 @@ export default function InputOffer({ offer_id, toggle }) {
       setSelectedExCurrency(offer.exchange_rate?.ex_currency);
       // alert(parseFloat(offer.exchange_rate?.ex_rate))
       setCreateFR(offer?.offer_retrocedent ? true : false);
+      setValue(
+        "no_of_instalments",
+        selectedPayment_type === "instalment"
+          ? JSON.parse(offer?.offer_detail?.payment_type)["instalment"]
+          : 1
+      );
     }
   }, [offer]);
 
@@ -254,7 +260,7 @@ export default function InputOffer({ offer_id, toggle }) {
     <form onSubmit={handleSubmit(handleCreateOffer)} ref={formRef}>
       <div className={styles.card_header}>
         <h2 className={styles.card_title}>Create facultative placement slip</h2>
-        <p>{Object.keys(JSON.parse(offer?.offer_detail?.payment_type))[0]}</p>
+        <p>{Object.values(JSON.parse(offer?.offer_detail?.payment_type))[0]}</p>
       </div>
       <div className={styles.card_body}>
         <div className="row">
