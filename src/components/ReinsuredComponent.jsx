215f48ca9c7bc715158edff9b0806cc34837f79e
Khoby-790 <ebaidoo79@gmail.com>
2021-05-23 14:20:16 +0000

    --- Auto-Git Commit ---

---
+++ src/components/ReinsuredComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ReinsuredComponent = ({ name }) => (
+  <>
+    {name}
+    <span className="text-danger font-size-16 font-weight-bold">*</span>
+  </>
+);
+
+export default ReinsuredComponent;
