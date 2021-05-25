cae4eed2323a8ee655ee09a012189ba8b4f002af
Khoby-790 <ebaidoo79@gmail.com>
2021-05-25 11:17:59 +0000

    --- Auto-Git Commit ---

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -2,11 +2,11 @@ export const PROTOCOL =
   process.env.NODE_ENV === 'development' ? 'http://' : 'https://';
 export const DOMAIN =
   process.env.NODE_ENV === 'development'
-    ? '192.168.100.16:8000'
+    ? '192.168.100.7:8000'
     : 'manage.kekrebrokerapp.com';
 export const COOKIE_NAME_AUTH_TOKEN = 'kek_re_auth_token';
 export const COOKIE_NAME_AUTH = 'kek_re_auth';
 export const PUSHER_KEY = '8fdc449c93173e31d8a2';
 export const PUSHER_CLUSTER = 'eu';
 export const CHANNEL = 'a2VrcmUtcHJpdmF0ZS1jaGFubmVs';
-export const EVENT = 'a2VrcmUtcHJpdmF0ZS1jaGFubmVs-event';
+export const EVENT = 'a2VrcmUtcHJpdmF0ZS1jaGFubmVs-event'; 
