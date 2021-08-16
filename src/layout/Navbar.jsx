fed1dcb2fb7e45b2a76c1cb84dd44f53813af070
Khoby-790 <ebaidoo79@gmail.com>
2021-08-16 13:43:14 +0000

    Modified src/layout/Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,89 +1,91 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import NotifcIcon from "./NotifcIcon";
 
-
 export const Navbar = () => {
-    const { state, signOut } = useContext(AuthContext);
-    return (
-        <header id="page-topbar">
-            <div className="navbar-header">
-                <div className="d-flex">
-                    <div style={{ backgroundColor: "#273B97" }} className="navbar-brand-box">
-                        <Link to="/admin/" className="logo logo-light">
-                            <span className="logo-sm">
-                                <img
-                                    src={require("../assets/visal-sm-logo.png")}
-                                    alt=""
-                                    height="40"
-                                    width="40"
-                                />
-                            </span>
-                            <span className="logo logo-lg">
-                                <img
-                                    src={require("../assets/visal-logo-big.png")}
-                                    alt=""
-                                    width="200"
-                                    height="60"
-                                />
-                            </span>
-                        </Link>
-                    </div>
+  const { state, signOut } = useContext(AuthContext);
+  return (
+    <header id="page-topbar">
+      <div className="navbar-header">
+        <div className="d-flex">
+          <div
+            style={{ backgroundColor: "#273B97" }}
+            className="navbar-brand-box"
+          >
+            <Link to="/admin/" className="logo logo-light">
+              <span className="logo-sm">
+                <img
+                  src={require("../assets/visal-sm-logo.png")}
+                  alt=""
+                  height="40"
+                  width="40"
+                />
+              </span>
+              <span className="logo logo-lg">
+                <img
+                  src={require("../assets/visal-logo-big.png")}
+                  alt=""
+                  width="200"
+                  height="60"
+                />
+              </span>
+            </Link>
+          </div>
 
-                    <button
-                        type="button"
-                        className="btn btn-sm px-3 font-size-16 header-item waves-effect"
-                        id="vertical-menu-btn"
-                    >
-                        <i className="fa fa-fw fa-bars"></i>
-                    </button>
-                </div>
+          <button
+            type="button"
+            className="btn btn-sm px-3 font-size-16 header-item waves-effect"
+            id="vertical-menu-btn"
+          >
+            <i className="fa fa-fw fa-bars"></i>
+          </button>
+        </div>
 
-                <div className="d-flex">
-                    <div className="dropdown d-none d-lg-inline-block ml-1">
-                        <button
-                            type="button"
-                            className="btn header-item noti-icon waves-effect"
-                            data-toggle="fullscreen"
-                        >
-                            <i className="bx bx-fullscreen"></i>
-                        </button>
-                    </div>
+        <div className="d-flex">
+          <div className="dropdown d-none d-lg-inline-block ml-1">
+            <button
+              type="button"
+              className="btn header-item noti-icon waves-effect"
+              data-toggle="fullscreen"
+            >
+              <i className="bx bx-fullscreen"></i>
+            </button>
+          </div>
 
-                    <NotifcIcon state={state} />
+          <NotifcIcon state={state} />
 
-                    <div className="dropdown d-inline-block dropright">
-                        <button
-                            type="button"
-                            className="btn header-item waves-effect"
-                            id="page-header-user-dropdown"
-                            data-toggle="dropdown"
-                            aria-haspopup="true"
-                            aria-expanded="false"
-                            style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                alignItems: "center",
-                            }}
-                        >
-                            <span className="rounded-circle header-profile-user d-flex justify-content-center align-items-center  rounded-circle bg-soft-primary text-primary font-size-12">
-                                {state.user?.employee?.emp_abbrv}
-                            </span>
-                            <span className="d-none d-xl-inline-block ml-1">
-                                {state.user?.employee?.employee_first_name}
-                            </span>
-                            <i className="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
-                        </button>
-                        <div className="dropdown-menu dropdown-menu-left">
-                            <Link
-                                to={{ pathname: "/admin/profile" }}
-                                className="dropdown-item"
-                            >
-                                <i className="bx bx-user font-size-16 align-middle mr-1"></i>
-                    Profile
-                  </Link>
-                            {/* <Link
+          <div className="dropdown d-inline-block dropright">
+            <button
+              type="button"
+              className="btn header-item waves-effect"
+              id="page-header-user-dropdown"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <span className="rounded-circle header-profile-user d-flex justify-content-center align-items-center  rounded-circle bg-soft-primary text-primary font-size-12">
+                {state.user?.employee?.emp_abbrv}
+              </span>
+              <span className="d-none d-xl-inline-block ml-1">
+                {state.user?.employee?.employee_first_name}
+              </span>
+              <i className="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+            </button>
+            <div className="dropdown-menu dropdown-menu-left">
+              <Link
+                to={{ pathname: "/admin/profile" }}
+                className="dropdown-item"
+              >
+                <i className="bx bx-user font-size-16 align-middle mr-1"></i>
+                Profile
+              </Link>
+              {/* <Link
                                 to={{ pathname: "/admin/settings" }}
                                 className="dropdown-item"
                             >
@@ -91,18 +93,15 @@ export const Navbar = () => {
                                 Settings
                             </Link> */}
 
-                            <div className="dropdown-divider"></div>
-                            <button
-                                onClick={signOut}
-                                className="dropdown-item text-danger"
-                            >
-                                <i className="bx bx-power-off font-size-16 align-middle mr-1 text-danger"></i>
-                    Logout
-                  </button>
-                        </div>
-                    </div>
-                </div>
+              <div className="dropdown-divider"></div>
+              <button onClick={signOut} className="dropdown-item text-danger">
+                <i className="bx bx-power-off font-size-16 align-middle mr-1 text-danger"></i>
+                Logout
+              </button>
             </div>
-        </header>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
