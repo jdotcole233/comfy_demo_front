e37f409ddbfb9ed2e4f8957388394aa40ace0f95
Khoby-790 <ebaidoo79@gmail.com>
2021-08-16 14:25:15 +0000

    Modified src/pages/Insurers/InsurersDetail.jsx

--- src/pages/Insurers/InsurersDetail.jsx
+++ src/pages/Insurers/InsurersDetail.jsx
@@ -22,13 +22,18 @@ import OfferListing from "../CreateSlip/OfferListing";
 import { useMemo } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useInsurer } from "../../context/InsurerProvider";
+import { useDispatch, useSelector } from "react-redux";
 import _ from "lodash";
+import { CHANGE_INSURER_PAGE_TYPE } from "../../redux/types/InsurerTypes";
 
 const retrocedentFilter = (offer) => offer && _.isNull(offer.offer_retrocedent);
 
 function InsurerDetail() {
   const { state: ctx } = useContext(AuthContext);
   // console.log(ctx?.user)
+  const dispatch = useDispatch();
+  const type = useSelector((state) => state.insurer.type);
+  const granted = useSelector((state) => state.app.granted);
   const history = useHistory();
   const { state } = useLocation();
   const { insurer: _insurer } = useInsurer();
@@ -318,6 +323,13 @@ function InsurerDetail() {
       });
   };
 
+  const changePageType = (_type) => {
+    dispatch({
+      type: CHANGE_INSURER_PAGE_TYPE,
+      payload: _type,
+    });
+  };
+
   if (loading) return <Loader />;
 
   if (error) return null;
@@ -329,6 +341,37 @@ function InsurerDetail() {
           <div className="col-12">
             <div className="page-title-box d-flex align-items-center justify-content-between">
               <h4 className="mb-0 font-size-18">Inusrer Details</h4>
+
+              <div className="page-title-right row">
+                {granted && (
+                  <div className="btn-group mr-4">
+                    <div
+                      onClick={() => changePageType("Fac")}
+                      className={`btn ${
+                        type !== "Fac" ? "btn-secondary" : "btn-primary"
+                      } w-lg btn-sm`}
+                    >
+                      <span className="bx bx-archive-in mr-4"></span>
+                      Facultative
+                    </div>
+                    <div
+                      onClick={() => changePageType("Treaty")}
+                      className={`btn ${
+                        type !== "Treaty" ? "btn-secondary" : "btn-primary"
+                      } w-lg btn-sm`}
+                    >
+                      <span className="bx bx-receipt mr-4"></span>
+                      Treaty
+                    </div>
+                  </div>
+                )}
+                <ol className="breadcrumb m-0">
+                  <li className="breadcrumb-item">
+                    <Link to="/admin/insurers">Insurers</Link>
+                  </li>
+                  <li className="breadcrumb-item active">Profile</li>
+                </ol>
+              </div>
 
               <div className="page-title-right">
                 <ol className="breadcrumb m-0">
