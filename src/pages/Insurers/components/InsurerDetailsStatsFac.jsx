3c3c874bf32711f4890f2d85526bab67937292ca
Khoby-790 <ebaidoo79@gmail.com>
2021-08-17 14:02:51 +0000

    Modified src/pages/Insurers/components/InsurerDetailsStatsFac.jsx

--- src/pages/Insurers/components/InsurerDetailsStatsFac.jsx
+++ src/pages/Insurers/components/InsurerDetailsStatsFac.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { useInsurerProps } from "../providers/InsurerProvider";
 
 const InsurerDetailsStatsFac = ({ insurer }) => {
   const type = useSelector((state) => state.insurer.type);
