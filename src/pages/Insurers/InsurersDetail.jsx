7d26e5c12ae2b9fb972bf396bc0151ef8b64146b
Khoby-790 <ebaidoo79@gmail.com>
2021-08-17 11:10:21 +0000

    Modified src/pages/Insurers/InsurersDetail.jsx

--- src/pages/Insurers/InsurersDetail.jsx
+++ src/pages/Insurers/InsurersDetail.jsx
@@ -29,6 +29,8 @@ import InsurerDetailOffers from "./components/InsurerDetailOffers";
 import InsurerDetialsTreaties from "./components/InsurerDetialsTreaties";
 import InsurerDetailsStatsFac from "./components/InsurerDetailsStatsFac";
 import InsurerDetailsStatsTreaty from "./components/InsurerDetailsStatsTreaty";
+import InsurerStatsFac from "./subComponents/InsurerStatsFac";
+import InsurerStatsTreaty from "./subComponents/InsurerStatsTreaty";
 
 const retrocedentFilter = (offer) => offer && _.isNull(offer.offer_retrocedent);
 
@@ -485,90 +487,12 @@ function InsurerDetail() {
           </div>
 
           <div className="col-xl-8">
-            <div className="row">
-              <div className="col-md-4">
-                <div className="card mini-stats-wid">
-                  <div className="card-body">
-                    <div className="media">
-                      <div className="media-body">
-                        <p className="text-muted font-weight-medium">
-                          Closed Offers
-                        </p>
-                        <h4 className="mb-0">
-                          {insurer?.insurer.insurer_overview?.total_closed}
-                        </h4>
-                      </div>
-
-                      <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
-                        <span className="avatar-title">
-                          <i className="bx bx-check-circle font-size-24"></i>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="card mini-stats-wid">
-                  <div className="card-body">
-                    <div className="media">
-                      <div className="media-body">
-                        <p className="text-muted font-weight-medium">
-                          Pending Offers
-                        </p>
-                        <h4 className="mb-0">
-                          {insurer?.insurer.insurer_overview?.total_pending}
-                        </h4>
-                      </div>
-
-                      <div className="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
-                        <span className="avatar-title">
-                          <i className="bx bx-hourglass font-size-24"></i>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="card mini-stats-wid">
-                  <div className="card-body">
-                    <div className="media">
-                      <div className="media-body">
-                        <p className="text-muted font-weight-medium">
-                          Unpaid Offers
-                        </p>
-                        <h4 className="mb-0">
-                          {insurer?.insurer.insurer_overview?.total_unpaid}
-                        </h4>
-                      </div>
-
-                      <div className="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
-                        <span className="avatar-title">
-                          <i className="bx bx-money font-size-24"></i>
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <OverViewCard
-                title="Total Fac. Premium"
-                value={JSON.parse(
-                  insurer?.insurer.insurer_overview?.total_fac_premium
-                )}
-                className="col-md-12"
-              />
-              <OverViewCard
-                title="Total Brokerage"
-                value={JSON.parse(
-                  insurer?.insurer.insurer_overview?.total_brokerage_amt
-                )}
-                className="col-md-12"
-              />
+            <div className="col-xl-8">
+              <InsurerDetailsStatsFac insurer={insurer} />
+              <InsurerDetailsStatsTreaty insurer={insurer} />
+              <InsurerStatsFac insurer={insurer?.insurer} />
+              <InsurerStatsTreaty insurer={insurer?.insurer} />
             </div>
-            <InsurerDetailsStatsFac insurer={insurer} />
-            <InsurerDetailsStatsTreaty insurer={insurer} />
           </div>
         </div>
         <BrokerageComponent insurer={insurer} />
