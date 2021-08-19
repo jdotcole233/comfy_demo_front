9b0794335663361d0a3b30cf7cdbbc25f97e8fd1
Khoby-790 <ebaidoo79@gmail.com>
2021-08-19 13:35:38 +0000

    Modified src/pages/ReInsurers/ReinsurerDetail.jsx,Modified src/pages/ReInsurers/components/ReinsurerDetailsFacStats.jsx

--- src/pages/ReInsurers/components/ReinsurerDetailsFacStats.jsx
+++ src/pages/ReInsurers/components/ReinsurerDetailsFacStats.jsx
@@ -1,105 +1,117 @@
-import React, { Fragment } from 'react'
-import { useSelector } from 'react-redux'
-import { CurrencyValues } from '../../../components'
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { CurrencyValues } from "../../../components";
 
 const ReinsurerDetailsFacStats = ({ overview }) => {
-    const type = useSelector(state => state.reinsurer.type)
-    return type === "Fac" ? (
-        <Fragment>
-            <div className="row">
-                <div className="col-md-6">
-                    <div className="card mini-stats-wid">
-                        <div className="card-body">
-                            <div className="media">
-                                <div className="media-body">
-                                    <p className="text-muted font-weight-medium">Closed Offers</p>
-                                    <h4 className="mb-0">{overview?.total_closed}</h4>
-                                </div>
-
-                                <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
-                                    <span className="avatar-title">
-                                        <i className="bx bx-check-circle font-size-24"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+  const type = useSelector((state) => state.reinsurer.type);
+  return type === "Fac" ? (
+    <Fragment>
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card mini-stats-wid">
+            <div className="card-body">
+              <div className="media">
+                <div className="media-body">
+                  <p className="text-muted font-weight-medium">Closed Offers</p>
+                  <h4 className="mb-0">{overview?.total_closed}</h4>
                 </div>
-                <div className="col-md-6">
-                    <div className="card mini-stats-wid">
-                        <div className="card-body">
-                            <div className="media">
-                                <div className="media-body">
-                                    <p className="text-muted font-weight-medium">Pending Offers</p>
-                                    <h4 className="mb-0">{overview?.total_pending}</h4>
-                                </div>
 
-                                <div className="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
-                                    <span className="avatar-title">
-                                        <i className="bx bx-hourglass font-size-24"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
+                  <span className="avatar-title">
+                    <i className="bx bx-check-circle font-size-24"></i>
+                  </span>
                 </div>
-                <div className="col-md-12">
-                    <div className="card mini-stats-wid">
-                        <div className="card-body">
-                            <div className="media">
-                                <div className="media-body">
-                                    <p className="text-muted font-weight-medium">Total Revenue</p>
-                                    <CurrencyValues data={JSON.parse(overview?.total_fac_premium || null)} />
-                                </div>
-
-                                <div className="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
-                                    <span className="avatar-title">
-                                        <i className="bx bx-money  font-size-24"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="card mini-stats-wid">
-                        <div className="card-body">
-                            <div className="media">
-                                <div className="media-body">
-                                    <p className="text-muted font-weight-medium">Total Withholding Tax</p>
-                                    <CurrencyValues data={JSON.parse(overview?.total_withholding_tax || null)} />
-                                </div>
-
-                                <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
-                                    <span className="avatar-title">
-                                        <i className="bx bx-money font-size-24"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="card mini-stats-wid">
-                        <div className="card-body">
-                            <div className="media">
-                                <div className="media-body">
-                                    <p className="text-muted font-weight-medium">Total NIC Levy</p>
-                                    <CurrencyValues data={JSON.parse(overview?.total_nic_tax || null)} />
-                                </div>
-
-                                <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
-                                    <span className="avatar-title">
-                                        <i className="bx bx-money font-size-24"></i>
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              </div>
             </div>
-        </Fragment>
-    ) : null
-}
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card mini-stats-wid">
+            <div className="card-body">
+              <div className="media">
+                <div className="media-body">
+                  <p className="text-muted font-weight-medium">
+                    Pending Offers
+                  </p>
+                  <h4 className="mb-0">{overview?.total_pending}</h4>
+                </div>
 
-export default ReinsurerDetailsFacStats
+                <div className="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
+                  <span className="avatar-title">
+                    <i className="bx bx-hourglass font-size-24"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-12">
+          <div className="card mini-stats-wid">
+            <div className="card-body">
+              <div className="media">
+                <div className="media-body">
+                  <p className="text-muted font-weight-medium">Total Revenue</p>
+                  <CurrencyValues
+                    data={JSON.parse(overview?.total_fac_premium || null)}
+                  />
+                </div>
+
+                <div className="avatar-sm align-self-center mini-stat-icon rounded-circle bg-primary">
+                  <span className="avatar-title">
+                    <i className="bx bx-money  font-size-24"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card mini-stats-wid">
+            <div className="card-body">
+              <div className="media">
+                <div className="media-body">
+                  <p className="text-muted font-weight-medium">
+                    Total Withholding Tax
+                  </p>
+                  <CurrencyValues
+                    data={JSON.parse(overview?.total_withholding_tax || null)}
+                  />
+                </div>
+
+                <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
+                  <span className="avatar-title">
+                    <i className="bx bx-money font-size-24"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="card mini-stats-wid">
+            <div className="card-body">
+              <div className="media">
+                <div className="media-body">
+                  <p className="text-muted font-weight-medium">
+                    Total NIC Levy
+                  </p>
+                  <CurrencyValues
+                    data={JSON.parse(overview?.total_nic_tax || null)}
+                  />
+                </div>
+
+                <div className="mini-stat-icon avatar-sm align-self-center rounded-circle bg-primary">
+                  <span className="avatar-title">
+                    <i className="bx bx-money font-size-24"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  ) : null;
+};
+
+export default ReinsurerDetailsFacStats;
