a8fe3396bda1d9cbae59c613aba7542ff4184bcc
Khoby-790 <ebaidoo79@gmail.com>
2021-08-27 00:34:19 +0000

    Made some changes to the Config

--- src/graphql/config.js
+++ src/graphql/config.js
@@ -2,7 +2,7 @@ export const PROTOCOL =
   process.env.NODE_ENV === 'development' ? 'http://' : 'https://';
 export const DOMAIN =
   process.env.NODE_ENV === 'development'
-    ? '7da1-41-210-15-166.ngrok.io'
+    ? '4f6e-41-210-10-74.ngrok.io'
     : 'manage.kekrebrokerapp.com';
 export const COOKIE_NAME_AUTH_TOKEN = 'kek_re_auth_token';
 export const COOKIE_NAME_AUTH = 'kek_re_auth';
