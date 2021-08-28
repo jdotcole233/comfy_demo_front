4f9e7ed4de65c88f50c583533b099b33fb64fcea
Khoby-790 <ebaidoo79@gmail.com>
2021-08-28 00:01:17 +0000

    Changes made here and there

--- src/pages/Employees/Employee.jsx
+++ src/pages/Employees/Employee.jsx
@@ -51,30 +51,58 @@ export default function Employees({ data, openViewEmployee }) {
   };
 
   const handleRemoveEmployee = () => {
-    swall
-      .fire({
-        icon: "warning",
-        allowOutsideClick: false,
-        allowEscapeKey: false,
-        title: "Delete Employee",
-        text: `This action would completely remove employee details from system`,
-        buttons: ["No", { text: "Yes", closeModal: false }],
-        showCancelButton: true,
-        confirmButtonText: "Yes",
-        cancelButtonText: "No",
-        reverseButtons: true,
-        showLoaderOnConfirm: true,
-        preConfirm: () => {
-          return removeEmployee()
-            .then((res) => res)
-            .catch((err) => new Error(err));
-        },
-      })
-      .then((input) => {
-        if (!input.isConfirmed) return;
-        swall.fire("Success", "Employee deleted successfully", "success");
-      })
-      .catch((err) => swall.fire("Whoops!!", "Something went wrong", "error"));
+    // swal({
+    //   icon: "warning",
+    //   allowOutsideClick: false,
+    //   allowEscapeKey: false,
+    //   title: "Delete Employee",
+    //   text: `This action would completely remove employee details from system`,
+    //   buttons: ["No", { text: "Yes", closeModal: false }],
+    //   showCancelButton: true,
+    //   confirmButtonText: "Yes",
+    //   cancelButtonText: "No",
+    //   reverseButtons: true,
+    //   showLoaderOnConfirm: true,
+    //   preConfirm: () => {
+    //     return removeEmployee()
+    //       .then((res) => res)
+    //       .catch((err) => new Error(err));
+    //   },
+    // })
+    //   .then((input) => {
+    //     if (!input.isConfirmed) return;
+    //     swall.fire("Success", "Employee deleted successfully", "success");
+    //   })
+    //   .catch((err) => swall.fire("Whoops!!", "Something went wrong", "error"));
+
+    swal({
+      closeOnClickOutside: false,
+      closeOnEsc: false,
+      icon: "warning",
+      title: "Delete Employee",
+      text: `This action would completely remove employee details from system`,
+      buttons: ["No", { text: "Yes", closeModal: false }],
+    }).then((input) => {
+      if (!input) throw null;
+      removeEmployee()
+        .then((res) => {
+          swal(
+            "Success",
+            "Employee deleted successfully",
+            "success",
+            "success"
+          );
+        })
+        .catch((err) => {
+          if (err) {
+            // console.log(err)
+            swal("Oh noes!", "The AJAX request failed!", "error");
+          } else {
+            swal.stopLoading();
+            swal.close();
+          }
+        });
+    });
   };
 
   const ips = data.log_activities.map(
@@ -210,7 +238,21 @@ export default function Employees({ data, openViewEmployee }) {
                 data-placement="top"
                 title="Activity Logs"
               >
-                <i className="bx bx-layer"></i>
+                {/* <i className="bx bx-layer"></i> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+                  />
+                </svg>
               </div>
             </div>
           </div>
