ee0b5a54be3f58f48cf8ef7a9b82689adb2c2650
Khoby-790 <ebaidoo79@gmail.com>
2021-09-03 04:15:16 +0000

    Added src/pages/BrokerDetails/components/BrokerDetailTreaties.jsx

---
+++ src/pages/BrokerDetails/components/columns.js
@@ -0,0 +1,139 @@
+export const associatesColumnns = [
+    {
+        label: "Name",
+        field: "name",
+    },
+    {
+        label: "Phone number(s)",
+        field: "phone",
+    },
+    {
+        label: "Email",
+        field: "email",
+    },
+    {
+        label: "Position",
+        field: "position",
+    },
+    {
+        label: "Actions",
+        field: "actions",
+    },
+];
+
+export const offersColumns = [
+    {
+        label: "Policy #",
+        field: "policy_no",
+    },
+    {
+        label: "Reinsured",
+        field: "company",
+    },
+    {
+        label: "Class of Business",
+        field: "cob",
+    },
+    {
+        label: "Insured",
+        field: "insured",
+    },
+    {
+        label: "Participating %",
+        field: "participating_percentage",
+    },
+    {
+        label: "Fac. Sum Insured",
+        field: "fac_sum_insured",
+    },
+    {
+        label: "Fac. Premium",
+        field: "fac_premium",
+    },
+    {
+        label: "Brokerage %",
+        field: "brokerage",
+    },
+    {
+        label: "Offer Status",
+        field: "offer_status",
+    },
+    {
+        label: "Payment Status",
+        field: "payment_status",
+    },
+    {
+        label: "Offer Date",
+        field: "offer_date",
+    },
+    {
+        label: "Actions",
+        field: "actions",
+    },
+];
+
+
+
+
+export const paymentsColumns = [
+    {
+        label: "Payment Type",
+        field: "type"
+    },
+    {
+        label: "Bank Name",
+        field: "bank_name"
+    },
+    {
+        label: "Beneficiary Bank Name",
+        field: "beneficiary_bank"
+    },
+    {
+        label: "Payment Amt",
+        field: "payment_amount"
+    },
+    {
+        label: "Payment Date",
+        field: "created_at"
+    },
+    {
+        label: "Last Payment Upate",
+        field: "updated_at"
+    },
+    {
+        label: "Actions",
+        field: "actions"
+    },
+]
+
+
+export const treatyColumns = [
+    {
+        label: "Ref#",
+        field: "treaty_reference",
+    },
+    {
+        label: "Treaty Program",
+        field: "treaty_program",
+    },
+    {
+        label: "Program Type",
+        field: "program_type",
+    },
+    {
+        label: "Employee",
+        field: "employee",
+    },
+    {
+        label: "Period",
+        field: "period",
+    },
+    {
+        label: "Payment Status",
+        field: "treaty_payment_status",
+    },
+    {
+        label: "Actions",
+        field: "actions",
+    },
+];
