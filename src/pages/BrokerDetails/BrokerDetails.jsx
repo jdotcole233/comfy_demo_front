d2f4951c4d01fe37ed918f87987ec994f30549b4
Khoby-790 <ebaidoo79@gmail.com>
2021-09-03 03:52:15 +0000

    Modified src/pages/BrokerDetails/BrokerDetails.jsx

--- src/pages/BrokerDetails/BrokerDetails.jsx
+++ src/pages/BrokerDetails/BrokerDetails.jsx
@@ -5,6 +5,13 @@ const BrokerDetails = () => {
   return (
     <div className="page-content">
       <BrokerDetailHeader />
+      <div className="row">
+        <div className="col-xl-4">
+          {/* Welcome */}
+
+          {/* Other Section */}
+        </div>
+      </div>
     </div>
   );
 };
