0db9bbc63c488eac7d5a81443e199f130ff1589f
Khoby-790 <ebaidoo79@gmail.com>
2021-09-03 03:49:16 +0000

    Modified src/pages/BrokerDetails/BrokerDetails.jsx

--- src/pages/BrokerDetails/BrokerDetails.jsx
+++ src/pages/BrokerDetails/BrokerDetails.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const BrokerDetails = () => {
-  return <div className="page-content">Broker Details</div>;
+  return (
+    <div className="page-content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <div className="page-title-box d-flex align-items-center justify-content-between">
+              <h4 className="mb-0 font-size-18">Broker Deatil</h4>
+
+              <div className="page-title-right row">
+                {/* {granted && (
+                  <div className="btn-group mr-4">
+                    <div
+                      onClick={() => changePageType("Fac")}
+                      className={`btn ${
+                        type !== "Fac" ? "btn-secondary" : "btn-primary"
+                      } w-lg btn-sm`}
+                    >
+                      <span className="bx bx-archive-in mr-4"></span>
+                      Facultative
+                    </div>
+                    <div
+                      onClick={() => changePageType("Treaty")}
+                      className={`btn ${
+                        type !== "Treaty" ? "btn-secondary" : "btn-primary"
+                      } w-lg btn-sm`}
+                    >
+                      <span className="bx bx-receipt mr-4"></span>
+                      Treaty
+                    </div>
+                  </div>
+                )} */}
+                <ol className="breadcrumb m-0">
+                  <li className="breadcrumb-item">
+                    <Link to="/admin/re-insurers">Re-Insurers</Link>
+                  </li>
+                  <li className="breadcrumb-item active">Profile</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BrokerDetails;
