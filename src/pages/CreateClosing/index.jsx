c0650f9ee79b96f2bb619b59fb62b651464cd7b3
Khoby-790 <ebaidoo79@gmail.com>
2021-09-23 13:28:28 +0000

    Changes made to effect the grouping on the create closing

--- src/pages/CreateClosing/index.jsx
+++ src/pages/CreateClosing/index.jsx
@@ -1,99 +1,103 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState, memo, useMemo } from 'react'
-import { Loader } from '../../components'
-import { useQuery } from 'react-apollo';
-import { ALLOFFERS, OFFERS } from '../../graphql/queries';
-import { columns } from './columns';
-import { generateClosingOffers } from './actions.js'
-import OfferOverView from './OfferOverView';
-import OfferListing from '../CreateSlip/OfferListing';
-import Header from './Header';
-
+import React, { useState, memo, useMemo } from "react";
+import { Loader } from "../../components";
+import { useQuery } from "react-apollo";
+import { ALLOFFERS, OFFERS } from "../../graphql/queries";
+import { columns } from "./columns";
+import { generateClosingOffers } from "./actions.js";
+import OfferOverView from "./OfferOverView";
+import OfferListing from "./OfferListing";
+import Header from "./Header";
 
 function ClosingOffer() {
-    const [offerOverview, setOfferOverview] = useState(null);
-    const [skip] = useState(0)
+  const [offerOverview, setOfferOverview] = useState(null);
+  const [skip] = useState(0);
 
-    const { data: offers, loading } = useQuery(OFFERS, {
-        variables: {
-            offer_status: ["CLOSED"]
-        },
-        fetchPolicy: "network-only",
-        // pollInterval: 1000
-    });
+  const { data: offers, loading } = useQuery(OFFERS, {
+    variables: {
+      offer_status: ["CLOSED"],
+    },
+    fetchPolicy: "network-only",
+    // pollInterval: 1000
+  });
 
-    const { data: alloffers, fetchMore, loading: fetching } = useQuery(ALLOFFERS, {
-        variables: {
-            offer_status: ["CLOSED"],
-            skip
-        },
-        fetchPolicy: "cache-and-network"
-    });
+  const {
+    data: alloffers,
+    fetchMore,
+    loading: fetching,
+  } = useQuery(ALLOFFERS, {
+    variables: {
+      offer_status: ["CLOSED"],
+      skip,
+    },
+    fetchPolicy: "cache-and-network",
+  });
 
+  const allTotal = useMemo(() => alloffers?.offers_all?.total, [alloffers]);
 
-    const allTotal = useMemo(() => alloffers?.offers_all?.total, [alloffers])
-
-    useMemo(() => {
-        if (offers) {
-            setOfferOverview(JSON.parse(offers.offerOverview).offer_overview);
-        }
-    }, [offers])
-
-
-
-    const closedOffers = useMemo(() => generateClosingOffers({
-        arr: offers?.offers,
-    }), [offers])
-
-    const allClosedOffers = useMemo(() => generateClosingOffers({
-        arr: alloffers?.offers_all,
-    }), [alloffers])
-
-
-    const handleLoadMore = (skip) => {
-        fetchMore({
-            variables: {
-                offer_status: ["CLOSED"],
-                skip
-            },
-            updateQuery: (prev, { fetchMoreResult }) => {
-                if (!fetchMoreResult) return prev;
-                fetchMoreResult.offers_all.offers = [
-                    ...prev.offers_all.offers,
-                    ...fetchMoreResult.offers_all.offers
-                ];
-                return fetchMoreResult
-            }
-        })
+  useMemo(() => {
+    if (offers) {
+      setOfferOverview(JSON.parse(offers.offerOverview).offer_overview);
     }
+  }, [offers]);
 
+  const closedOffers = useMemo(
+    () =>
+      generateClosingOffers({
+        arr: offers?.offers,
+      }),
+    [offers]
+  );
 
+  const allClosedOffers = useMemo(
+    () =>
+      generateClosingOffers({
+        arr: alloffers?.offers_all,
+      }),
+    [alloffers]
+  );
 
-    return (
-        <>
-            {loading && <Loader />}
-            {!loading && offers && <div className="page-content">
-                <Header closedOffers={{ length: allTotal }} />
+  const handleLoadMore = (skip) => {
+    fetchMore({
+      variables: {
+        offer_status: ["CLOSED"],
+        skip,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+        fetchMoreResult.offers_all.offers = [
+          ...prev.offers_all.offers,
+          ...fetchMoreResult.offers_all.offers,
+        ];
+        return fetchMoreResult;
+      },
+    });
+  };
 
-                <OfferOverView offerOverview={offerOverview} />
+  return (
+    <>
+      {loading && <Loader />}
+      {!loading && offers && (
+        <div className="page-content">
+          <Header closedOffers={{ length: allTotal }} />
 
-                <OfferListing
-                    path="/admin/create-closing"
-                    title="Closed Offers"
-                    recent={closedOffers}
-                    all={allClosedOffers}
-                    handleLoadMore={handleLoadMore}
-                    fetching={fetching}
-                    allTotal={allTotal}
-                    setInputOffer={null}
-                    columns={columns}
-                />
+          <OfferOverView offerOverview={offerOverview} />
 
-
-
-            </div>}
-        </>
-    )
+          <OfferListing
+            path="/admin/create-closing"
+            title="Closed Offers"
+            recent={closedOffers}
+            all={allClosedOffers}
+            handleLoadMore={handleLoadMore}
+            fetching={fetching}
+            allTotal={allTotal}
+            setInputOffer={null}
+            columns={columns}
+          />
+        </div>
+      )}
+    </>
+  );
 }
 
-export default memo(ClosingOffer)
+export default memo(ClosingOffer);
