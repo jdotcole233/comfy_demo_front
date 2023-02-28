15bbb97011deda1f5baa8a990644e21f26f26f73
Khoby-790 <ebaidoo79@gmail.com>
2023-02-28 00:09:37 +0000

    chore: :fire: changes made to portfolio statment

--- src/pages/TreatyPrograms/components/EffectedWithListing.jsx
+++ src/pages/TreatyPrograms/components/EffectedWithListing.jsx
@@ -29,7 +29,7 @@ function EffectedWithListing({
   const data = _.mapValues(_.groupBy(reinsurers, "layer_number"), (list, key) =>
     list.map((item) => {
       // console.log(`Layer ${key}`,item.treaty_participant_deductions[0].nic_levy_amount);
-      return _.omit(item, "layer_number")
+      return _.omit(item, "layer_number");
     })
   );
 
@@ -66,163 +66,172 @@ function EffectedWithListing({
                 {expand ? "Collapse" : "Expand"}
               </button>
             </div>
-            {expand && <div className="card-body">
-              <div className="d-flex justify-content-between">
-                {/* <h4 className="card-title mb-4"></h4> */}
-                <div className="mb-4">
-                  {!isNonProp && (
-                    <PortfolioStatment
-                      id={
-                        treaty?.treaty_proportional_detail
-                          ?.treaty_proportional_detail_id
+            {expand && (
+              <div className="card-body">
+                <div className="d-flex justify-content-between">
+                  {/* <h4 className="card-title mb-4"></h4> */}
+                  <div className="mb-4">
+                    {!isNonProp && (
+                      <PortfolioStatment
+                        id={
+                          treaty?.treaty_proportional_detail
+                            ?.treaty_proportional_detail_id
+                        }
+                        size={treaty?.treaty_accounts?.length}
+                      />
+                    )}
+                    <button
+                      disabled={
+                        !treaty?.treaty_participants?.length && !hasAccounts
                       }
-                      size={treaty?.treaty_accounts?.length}
-                    />
-                  )}
-                  <button
-                    disabled={!treaty?.treaty_participants?.length && !hasAccounts}
-                    onClick={toggle}
-                    className="btn btn-sm w-md ml-2 btn-warning"
-                    title="This button allows you to specify deductions specific to reinsurers participating on this treaty"
-                  >
-                    Modify Deductions
-                  </button>
-                </div>
-              </div>
-              {treaty?.treaty_accounts?.length > 3 && (
-                <div className="border mb-3">
-                  <div className="alert alert-success">
-                    <span>
-                      Found Portfolio statement for{" "}
-                      {new Date(
-                        treaty?.treaty_proportional_detail?.created_at
-                      ).toDateString()}{" "}
-                      below
-                    </span>
+                      onClick={toggle}
+                      className="btn btn-sm w-md ml-2 btn-warning"
+                      title="This button allows you to specify deductions specific to reinsurers participating on this treaty"
+                    >
+                      Modify Deductions
+                    </button>
                   </div>
-                  <table className="table tab">
-                    <thead>
-                      <tr>
-                        <th>Date</th>
-                        <th>Overall Gross Premium</th>
-                        <th>Action</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>
-                          {new Date(
-                            treaty?.treaty_proportional_detail?.created_at
-                          ).toDateString()}
-                        </td>
-                        <td>
-                          {
-                            treaty?.treaty_proportional_detail
-                              ?.overall_gross_premium
-                          }
-                        </td>
-                        <td>
-                          <UpdatePortfolioStatment
-                            detail={treaty?.treaty_proportional_detail}
-                          />
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
                 </div>
-              )}
-              <div className="d-flex flex-row align-items-center mb-2 justify-content-between">
-                <h5
-                  style={{
-                    fontSize: 12,
-                    color:
-                      remainingPercentage[currentIndex] > 0
-                        ? "#DB5461"
-                        : "#F46A6A",
-                  }}
-                >
-                  Current Quota: {remainingPercentage[currentIndex]}%
-                </h5>
-                <div style={{ width: 80, height: 80 }}>
-                  <CircularProgressbarWithChildren
-                    value={remainingPercentage[currentIndex]}
-                    strokeWidth={10}
-                    maxValue={parseFloat(treaty?.order_hereon)}
-                    // text={`${showingFacOffer.toFixed(5)}%`}
-                    styles={buildStyles({
-                      // Rotation of path and trail, in number of turns (0-1)
-                      rotation: 0.25,
-
-                      // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-                      strokeLinecap: "butt",
-
-                      // Text size
-                      textSize: "16px",
-
-                      // How long animation takes to go from one percentage to another, in seconds
-                      pathTransitionDuration: 0.5,
-
-                      // Can specify path transition in more detail, or remove it entirely
-                      // pathTransition: 'none',
-
-                      // Colors
-                      pathColor:
+                {treaty?.treaty_accounts?.length > 3 && (
+                  <div className="border mb-3">
+                    <div className="alert alert-success">
+                      <span>
+                        Found Portfolio statement for{" "}
+                        {new Date(
+                          treaty?.treaty_proportional_detail?.created_at
+                        ).toDateString()}{" "}
+                        below
+                      </span>
+                    </div>
+                    <table className="table tab">
+                      <thead>
+                        <tr>
+                          <th>Date</th>
+                          <th>Overall Gross Premium</th>
+                          <th>Action</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                            {new Date(
+                              treaty?.treaty_proportional_detail?.created_at
+                            ).toDateString()}
+                          </td>
+                          <td>
+                            {
+                              treaty?.treaty_proportional_detail
+                                ?.overall_gross_premium
+                            }
+                          </td>
+                          <td>
+                            <UpdatePortfolioStatment
+                              detail={treaty?.treaty_proportional_detail}
+                            />
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                )}
+                <div className="d-flex flex-row align-items-center mb-2 justify-content-between">
+                  <h5
+                    style={{
+                      fontSize: 12,
+                      color:
                         remainingPercentage[currentIndex] > 0
                           ? "#DB5461"
                           : "#F46A6A",
-                      textColor: "#F46A6A",
-                      trailColor: "#d6d6d6",
-                      backgroundColor: "#3e98c7",
-                    })}
+                    }}
                   >
-                    <div
-                      style={{
-                        fontSize: 12,
-                        marginTop: -5,
-                        color:
+                    Current Quota: {remainingPercentage[currentIndex]}%
+                  </h5>
+                  <div style={{ width: 80, height: 80 }}>
+                    <CircularProgressbarWithChildren
+                      value={remainingPercentage[currentIndex]}
+                      strokeWidth={10}
+                      maxValue={parseFloat(treaty?.order_hereon)}
+                      // text={`${showingFacOffer.toFixed(5)}%`}
+                      styles={buildStyles({
+                        // Rotation of path and trail, in number of turns (0-1)
+                        rotation: 0.25,
+
+                        // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                        strokeLinecap: "butt",
+
+                        // Text size
+                        textSize: "16px",
+
+                        // How long animation takes to go from one percentage to another, in seconds
+                        pathTransitionDuration: 0.5,
+
+                        // Can specify path transition in more detail, or remove it entirely
+                        // pathTransition: 'none',
+
+                        // Colors
+                        pathColor:
                           remainingPercentage[currentIndex] > 0
                             ? "#DB5461"
                             : "#F46A6A",
-                      }}
-                    >
-                      <strong>Quota</strong>
-                    </div>
-                  </CircularProgressbarWithChildren>
-                </div>
-              </div>
-              <ul className="nav nav-tabs nav-tabs-custom">
-                {isNonProp &&
-                  actualLayers.map((_, key) => (
-                    <li
-                      key={key}
-                      onClick={() => changeLayer(parseInt(_))}
-                      className="nav-item btn"
+                        textColor: "#F46A6A",
+                        trailColor: "#d6d6d6",
+                        backgroundColor: "#3e98c7",
+                      })}
                     >
                       <div
-                        className={`nav-link ${parseInt(_) === currentIndex ? "active" : ""
+                        style={{
+                          fontSize: 12,
+                          marginTop: -5,
+                          color:
+                            remainingPercentage[currentIndex] > 0
+                              ? "#DB5461"
+                              : "#F46A6A",
+                        }}
+                      >
+                        <strong>Quota</strong>
+                      </div>
+                    </CircularProgressbarWithChildren>
+                  </div>
+                </div>
+                <ul className="nav nav-tabs nav-tabs-custom">
+                  {isNonProp &&
+                    actualLayers.map((_, key) => (
+                      <li
+                        key={key}
+                        onClick={() => changeLayer(parseInt(_))}
+                        className="nav-item btn"
+                      >
+                        <div
+                          className={`nav-link ${
+                            parseInt(_) === currentIndex ? "active" : ""
                           }`}
-                        href="#"
-                      >{`Layer ${_}`}</div>
-                    </li>
-                  ))}
-              </ul>
-              <div className="mt-4">
-                <Datatable
-                  columns={reinsurersColumns}
-                  data={
-                    isNonProp
-                      ? _.uniqBy(data[`${currentIndex}`], "reinsurer_id")
-                      : reinsurers
-                  }
-                />
+                          href="#"
+                        >{`Layer ${_}`}</div>
+                      </li>
+                    ))}
+                </ul>
+                <div className="mt-4">
+                  <Datatable
+                    columns={reinsurersColumns}
+                    data={
+                      isNonProp
+                        ? _.uniqBy(data[`${currentIndex}`], "reinsurer_id")
+                        : reinsurers
+                    }
+                  />
+                </div>
               </div>
-            </div>
-            }
+            )}
           </div>
         </div>
       </div>
 
-      <Drawer isvisible={showDeductionDrawer} toggle={toggle} setShow={setShowDeductionDrawer} width="50%">
+      <Drawer
+        isvisible={showDeductionDrawer}
+        toggle={toggle}
+        setShow={setShowDeductionDrawer}
+        width="50%"
+      >
         <UpdateDeductions
           reinsurers={isNonProp ? data[`${currentIndex}`] : reinsurers}
           treaty_accounts={treaty?.treaty_accounts}
